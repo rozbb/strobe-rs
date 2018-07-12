169917269cfe0039f19e7e12167b62d65ba21239
Michael Rosenberg <micro@fastmail.com>
2018-07-12 18:23:26 -0400

    Added a simple benchmark

---
+++ benches/simple_bench.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+
+extern crate test;
+extern crate strobe_rs;
+
+use test::Bencher;
+
+use strobe_rs::{SecParam, Strobe};
+
+#[bench]
+fn simple_bench(b: &mut Bencher) {
+    let mut s = Strobe::new(b"simplebench".to_vec(), SecParam::B256);
+    b.iter(|| {
+        let mut v = vec![0u8; 256];
+        v = s.send_enc(v, None, false);
+        s.recv_enc(v, None, false)
+    });
+}
