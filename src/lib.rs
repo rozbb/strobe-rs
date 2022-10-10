74290067ad598f978e98ff5bc3abef024e319bf3
Michael Rosenberg <michael@mrosenberg.pub>
2022-10-10 16:24:28 -0400

    Improved docs and fixed docsrs build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+//! # strobe-rs
+//!
+//! This is a `no_std` implementation of the [Strobe protocol framework][strobe] in pure Rust. It
+//! is intended to be used as a library to build other protocols and frameworks. This
+//! implementation currently only supports Keccak-f\[1600\].
+//!
+//! Here is a simple program that encrypts and decrypts a message:
+//! ```
+//! # use strobe_rs::{SecParam, Strobe};
+//! # fn main() {
+//! // Transmitter initializes their STROBE instance with a public context string
+//! let mut tx = Strobe::new(b"correctnesstest", SecParam::B128);
+//! // Receiver initializes their STROBE instance with a public context string
+//! let mut rx = Strobe::new(b"correctnesstest", SecParam::B128);
+//!
+//! // Transmitter keys their instance
+//! tx.key(b"the-combination-on-my-luggage", false);
+//! // Receiver keys their instance
+//! rx.key(b"the-combination-on-my-luggage", false);
+//!
+//! // Transmitter encrypts a message in place
+//! let mut msg = b"Attack at dawn".to_vec();
+//! tx.send_enc(msg.as_mut_slice(), false);
+//!
+//! // Rename for clarity. `msg` has been encrypted in-place.
+//! let mut ciphertext = msg;
+//!
+//! // Receiver takes the message and decrypts it in place
+//! rx.recv_enc(ciphertext.as_mut_slice(), false);
+//!
+//! // Rename for clarity again
+//! let round_trip_msg = ciphertext;
+//!
+//! // Ensure that the sent message was the one received
+//! assert_eq!(&round_trip_msg, b"Attack at dawn");
+//! # }
+//! ```
+//!
+//! [strobe]: https://strobe.sourceforge.io/
+
+// The doc_cfg feature is only available in nightly. It lets us mark items in documentation as
+// dependent on specific features.
+#![cfg_attr(docsrs, feature(doc_cfg))]
 //-------- no_std stuff --------//
 #![no_std]
 
